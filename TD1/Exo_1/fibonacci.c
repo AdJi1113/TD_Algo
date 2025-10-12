f6d88c4b8232cd6a04b87fb286d832a2f89ddf99
AdJi1113 <bouchehitadnane@gmail.com>
2025-10-12 22:47:12 +0200

    Create fibonacci.c

---
+++ TD1/Exo_1/fibonacci.c
@@ -0,0 +1,20 @@
+#include "fibonacci.h"
+
+// Méthode naïve
+long long int fibonacci_naive(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+}
+
+// Version dynamique
+long long int fibonacci_dynamic(int n, long long int DPFib[]) {
+    if (n <= 1)
+        return n;
+
+    if (DPFib[n] != -1)
+        return DPFib[n];
+
+    DPFib[n] = fibonacci_dynamic(n - 1, DPFib) + fibonacci_dynamic(n - 2, DPFib);
+    return DPFib[n];
+}
